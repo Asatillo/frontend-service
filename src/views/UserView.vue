5ff227898537ede748d3498e546c581a109d1618
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-01-19 15:48:58 +0100

    add user page

---
+++ src/views/UserView.vue
@@ -0,0 +1,22 @@
+<template>
+    <Layout>
+      <User />
+    </Layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import User from '../components/User.vue';
+import Layout from '@/components/Layout.vue';
+
+export default defineComponent({
+  name: 'UserView',
+
+  components: {
+    User,
+    Layout
+},
+});
+</script>
