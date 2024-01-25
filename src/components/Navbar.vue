3ba1d03e3850f6578f4ad4d53c368bb5ccc67386
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-01-25 15:42:23 +0100

    change the avatar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
-<v-navigation-drawer expand-on-hover rail :elevation="2"
-    image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg">
+<v-navigation-drawer expand-on-hover rail :elevation="2" style="color: azure;"
+    image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg" >
     <v-list>
-        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" :title="fullname"
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/3.jpg" :title="fullname"
             :subtitle="email" @click="redirectToRoute('my-profile')"></v-list-item>
     </v-list>
 
     <v-divider></v-divider>
 
-    <v-list density="compact" nav>
+    <v-list density="compact" nav >
         <v-list-item @click="redirectToRoute(item.value)" v-for="item in menuItems" :key="item.value"
             :prepend-icon="item.icon" :title="item.title" :value="item.value"></v-list-item>
     <v-list-item @click="handleLogout" prepend-icon="mdi-logout" title="Logout"></v-list-item>
