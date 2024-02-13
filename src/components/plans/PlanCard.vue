45a8fe6de3101bc8350ee3276cb713af56082ad1
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-02-13 13:16:42 +0100

    move action menu up and connect to parent

--- src/components/plans/PlanCard.vue
+++ src/components/plans/PlanCard.vue
@@ -1,8 +1,25 @@
 <template>
     <v-card class="hover-card elevation-1 rounded-xl">
-        <v-card-title>
-            <v-icon size="smaller" class="mr-1" :color="plan.active ? 'green' : 'red'">mdi-circle</v-icon>
-            {{ plan.name }}
+        <v-card-title class="pr-0 d-flex justify-space-between align-center">
+            <v-container class="pa-0 text-truncate">
+                <v-icon size="smaller" :color="plan.active ? 'green' : 'red'">mdi-circle</v-icon>
+                <span class="ml-1">{{ plan.name }}</span>
+            </v-container>
+            <v-menu>
+                <template v-slot:activator="{ props }">
+                    <v-btn class="mr-1" icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+                </template>
+                <v-list class="pa-0">
+                    <v-list-item>
+                        <v-btn color="primary" @click="$emit('edit', plan)" width="100%">Edit</v-btn>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-btn :color="plan.active ? 'error' : 'green'" @click="$emit('changeActive', plan)">
+                            {{ plan.active ? 'Deactivate' : 'Activate' }}
+                        </v-btn>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
         </v-card-title>
         <v-card-subtitle>{{ plan.description }}</v-card-subtitle>
         <v-divider class="mt-1"></v-divider>
@@ -11,27 +28,15 @@
                 <v-list-item-title>{{ convertPeriodToDate(plan.duration) }}</v-list-item-title>
             </v-list-item>
             <v-list-item prepend-icon="mdi-cash-multiple">
-                <v-list-item-title>{{ plan.price }} HUF</v-list-item-title>
+                <v-list-item-title>{{ addCommasToPrice(plan.price) }} HUF</v-list-item-title>
             </v-list-item>
-            <v-list dense class="rounded-xl mt-2 pl-2 elevation-1">
-                <v-chip-group>
-                    <v-chip v-for="service in plan.services" :key="service.id" :value="service.name">
-                        <v-icon>{{ icons[service.type] }}</v-icon>
-                        <span> {{ service.name }}</span>
-                    </v-chip>
-                </v-chip-group>
-            </v-list>
+            <v-chip-group class="pl-2">
+                <v-chip v-for="service in plan.services" :key="service.id" :value="service.name">
+                    <v-icon>{{ icons[service.type] }}</v-icon>
+                    <span> {{ service.name }}</span>
+                </v-chip>
+            </v-chip-group>
         </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-            <v-container>
-                <v-row justify="center">
-                    <v-btn color="primary" @click="$emit('edit', plan)">Edit</v-btn>
-                    <v-btn :color="plan.active ? 'error' : 'green'" @click="$emit('changeActive', plan)">{{ plan.active ?
-                        'Deactivate' : 'Activate' }}</v-btn>
-                </v-row>
-            </v-container>
-        </v-card-actions>
     </v-card>
 </template>
 
@@ -39,6 +44,7 @@
 import { defineProps } from 'vue'
 import { ref } from 'vue'
 import { convertPeriodToDate } from '@/services/date-formatting'
+import { addCommasToPrice} from '@/services/number-formatting'
 
 defineProps({
     plan: Object
