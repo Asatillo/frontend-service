fc6d539a2a17b48b14c82121294404a5b0184775
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-02-27 09:24:31 +0100

    improve navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,14 @@
 <template>
-    <v-navigation-drawer v-model="drawer" permanent style="color: azure; background-color: rgb(58, 58, 58);" elevation="1"
-        :rail="rail" @click="rail = false">
+    <v-navigation-drawer v-model="drawer" permanent
+        style="color: azure; background-color: rgb(45, 47, 64); max-width: fit-content;" elevation="10" :rail="rail"
+        @click="rail = false">
         <v-list density="compact">
-            <v-list-item v-if="user" :prepend-avatar="user.imageUrl" :title="fullname" :subtitle="email"
-                @click="redirectToRoute('MyProfile')">
+            <v-list-item title="LOGO" @click="redirectToRoute('Home')">
                 <template v-slot:append>
-                    <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+                    <v-btn v-if="!rail" variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
                 </template>
             </v-list-item>
         </v-list>
-
         <v-divider></v-divider>
 
         <v-list :lines="false" density="compact" nav>
@@ -22,8 +21,7 @@
         </v-list>
 
         <template v-slot:append>
-            <v-list :lines="false" density="compact" nav>
-                <v-divider></v-divider>
+            <v-list :lines="false" density="compact" nav>     
                 <v-list-item @click="handleChangeTheme" :title="isDark ? 'Light mode' : 'Dark mode'">
                     <template v-slot:prepend>
                         <v-icon icon="mdi-theme-light-dark" size="18"></v-icon>
@@ -34,9 +32,11 @@
                         <v-icon icon="mdi-cog" size="18"></v-icon>
                     </template>
                 </v-list-item>
-                <v-list-item @click="handleLogout" title="Logout">
-                    <template v-slot:prepend>
-                        <v-icon icon="mdi-logout" size="18"></v-icon>
+                <v-divider thickness="3" class="pb-1"></v-divider>
+                <v-list-item v-if="user" :prepend-avatar="user.imageUrl" :title="user.firstName" :subtitle="email"
+                    @click="redirectToRoute('MyProfile')">
+                    <template v-slot:append>
+                        <v-btn class="ml-2" variant="text" @click.stop="handleLogout" icon="mdi-logout"></v-btn>
                     </template>
                 </v-list-item>
             </v-list>
@@ -52,7 +52,7 @@ import router from '@/router';
 const drawer = ref(true)
 const rail = ref(false)
 const menuItems = ref([
-    // { icon: 'mdi-view-dashboard', title: 'Home', value: '' },
+    { icon: 'mdi-view-dashboard', title: 'Home', value: '' },
     { icon: 'mdi-account-multiple', title: 'Customers', value: 'customers' },
     { icon: 'mdi-playlist-check', title: 'Subscriptions', value: 'subscriptions' },
     { icon: 'mdi-currency-usd', title: 'Sales', value: 'sales' },
