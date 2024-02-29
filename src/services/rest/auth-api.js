001f18cd5200716b198a407aa23e662b4f7aeff2
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-02-29 15:44:00 +0100

    improve auth-api requests

--- src/services/rest/auth-api.js
+++ src/services/rest/auth-api.js
@@ -1,34 +1,26 @@
 import http from '@/plugins/axios'
 
 export const authorizeUser = async (username, password) => {
-    try {
-        const userData = await http.post('auth-service/auth/authenticate', {
-            username: username,
-            password: password,
-        });
-        return userData.data
-    } catch (err) {
-        if (err.response) {
-            return err.response.data
-        }
-    }
+    return await http.post('auth-service/auth/authenticate', {
+        username: username,
+        password: password,
+    }).then(response => {
+        return response.data
+    }).catch(error => {
+        return error.response.data
+    })
 }
 
 export const signUpUser = async (userData) => {
-    try {
-        const response = await http.post('auth-service/auth/register', userData);
+    return await http.post('auth-service/auth/register', userData).then(response => {
         return response.data
-    } catch (err) {
-        if (err.response) {
-            return err.response.data
-        }
-    }
+    }).catch(error => {
+        return error.response.data
+    })
 }
 
 export const getUsersByRole = async ({ page, itemsPerPage}, role) => {
     return await http.get(`/auth-service/users?page=${page}&size=${itemsPerPage}&role=${role}`).then(response => {
         return response.data
-    }).catch(err => {
-        console.log(err)
     })
 }
