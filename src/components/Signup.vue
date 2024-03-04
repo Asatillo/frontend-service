8daab56aafb323a7cb14b8335e65a9779b521ab6
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-03-04 10:00:39 +0100

    improve signup page

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -4,32 +4,51 @@
       <v-container class="d-flex justify-center" style="max-width: 200px ">
         <v-img src="../assets/logo1.png" width="200"></v-img>
       </v-container>
-      <v-form @submit.prevent="handleSignup">
+      <v-card elevation="0">
+        <v-card-text>
+          <v-window v-model="step">
+            <v-window-item :value="1">
+              <v-text-field v-model="firstName" label="First name" :rules="nameRules">
+              </v-text-field>
 
-        <v-text-field v-model="firstName" label="First name" :rules="nameRules">
-        </v-text-field>
+              <v-text-field v-model="lastName" label="Last name" :rules="nameRules">
+              </v-text-field>
 
-        <v-text-field v-model="lastName" label="Last name" :rules="nameRules">
-        </v-text-field>
+              <v-text-field v-model="username" label="Username" :rules="usernameRules">
+              </v-text-field>
 
-        <v-text-field v-model="username" label="Username" :rules="usernameRules">
-        </v-text-field>
+              <v-text-field v-model="email" label="Email" :rules="emailRules">
+              </v-text-field>
+            </v-window-item>
+            <v-window-item :value="2">
+              <v-text-field v-model="password" label="Password" :rules="passwordRules"
+                :append-inner-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :type="show1 ? 'text' : 'password'"
+                @click:append-inner="show1 = !show1">
+              </v-text-field>
 
-        <v-text-field v-model="email" label="Email" :rules="emailRules">
-        </v-text-field>
-
-        <v-text-field v-model="password" label="Password" :rules="passwordRules"
-          :append-inner-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :type="show1 ? 'text' : 'password'"
-          @click:append-inner="show1 = !show1">
-        </v-text-field>
-
-        <v-text-field v-model="password2" label="Confirm password" :rules="confirmPasswordRules"
-          :append-inner-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'" :type="show2 ? 'text' : 'password'"
-          @click:append-inner="show2 = !show2">
-        </v-text-field>
-
-        <v-btn type="submit" color="primary" block class="mt-2" :disabled="!areAllRulesMet"> Sign up </v-btn>
-      </v-form>
+              <v-text-field v-model="password2" label="Confirm password" :rules="confirmPasswordRules"
+                :append-inner-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'" :type="show2 ? 'text' : 'password'"
+                @click:append-inner="show2 = !show2">
+              </v-text-field>
+            </v-window-item>
+            <v-window-item :value="3">
+              <div v-if="!responseObj.loading" class="pa-4 text-center">
+                <v-icon size="80" color="green">{{ mdi-check }}</v-icon>
+                <h3 class="text-h6 font-weight-light mb-2">{{ responseObj.success ? 'Success' :
+                  'Something went wrong' }}!</h3>
+                <span class="text-caption text-grey">{{ responseObj.message }}</span>
+              </div>
+              <v-progress-linear v-else indeterminate color="primary"></v-progress-linear>
+            </v-window-item>
+          </v-window>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn v-if="step == 1" color="primary" text @click="step++">Next</v-btn>
+          <v-btn v-if="step == 2" color="primary" text @click="step--">Back</v-btn>
+          <v-btn v-if="step == 2" color="primary" text @click="handleSignup" :disabled="!areAllRulesMet">Sign up</v-btn>
+        </v-card-actions>
+      </v-card>
       <div class="mt-2">
         <p class="text-body-2"> Already have an account? <router-link to="/login"> Sign in </router-link></p>
       </div> <br>
@@ -45,6 +64,7 @@ import { ref, computed } from 'vue';
 import { signUpUser } from '@/services/rest/auth-api';
 import router from '@/router';
 
+const step = ref(1);
 const show1 = ref(false);
 const show2 = ref(false);
 const username = ref('');
@@ -54,6 +74,8 @@ const email = ref('');
 const password = ref('');
 const password2 = ref('');
 const error = ref('');
+
+const responseObj = ref({ loading: false, success: false, message: '' });
 
 const nameRules = computed(() => [required(firstName.value), required(lastName.value)]);
 const usernameRules = computed(() => [required(username.value)]);
@@ -95,18 +117,26 @@ async function handleSignup() {
   }
 
   var userData = {
-      username: username.value,
-      firstName: firstName.value,
-      lastName: lastName.value,
-      password: password.value,
-      email: email.value,
-    }
+    username: username.value,
+    firstName: firstName.value,
+    lastName: lastName.value,
+    password: password.value,
+    email: email.value,
+  }
+  step.value = 3;
+  responseObj.value.loading = true;
   signUpUser(userData).then(response => {
+    responseObj.value.success = response.success;
+    responseObj.value.message = response.message;
+    responseObj.value.loading = false;
     if (response.token) {
       localStorage.setItem('accessToken', response.token);
       localStorage.setItem('user', JSON.stringify(response.user));
       router.push({ name: 'Home' });
     } else {
+      loading.value = false;
+      step.value--;
+      console.log(response);
       error.value = 'Authentication failed';
     }
   }).catch(error => {
@@ -114,3 +144,9 @@ async function handleSignup() {
   });
 }
 </script>
+
+<style scoped>
+.v-input{
+  padding: 1px 1px 1px 1px;
+}
+</style>
