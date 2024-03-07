344c4cde46d7c20fbc215562a03cd287c3316bd6
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-03-07 13:35:07 +0100

    fix search in customers page

--- src/components/Customers/Customers.vue
+++ src/components/Customers/Customers.vue
@@ -3,6 +3,7 @@
         <div class="text-h5">Customers</div>
     </v-container>
     <v-container>
+            <v-btn prepend-icon="mdi-plus" color="primary" @click="createNewCustomerDialog">New
         <v-data-table-server v-model:itemsPerPage="itemsPerPage" :items="customers" item-value="id"
             :items-length="totalItems" :items-per-page-options="[15, 20, 25]" :loading="loading"
             @update:options="getAllCustomers" :headers="headers" :search="search" @click:row="handleRowClick"
