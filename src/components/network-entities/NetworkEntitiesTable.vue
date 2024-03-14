b1230ecb753c7d18bd3df4d53ebc50898583136b
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-03-14 15:06:36 +0100

    move toolbar outside of the table

--- src/components/network-entities/NetworkEntitiesTable.vue
+++ src/components/network-entities/NetworkEntitiesTable.vue
@@ -1,14 +1,12 @@
 <template>
+    <v-toolbar flat>
+        <v-text-field class="ml-1" v-model="search" append-inner-icon="mdi-magnify" label="Search" single-line
+            hide-details></v-text-field>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-btn color="green" prepend-icon="mdi-sim" @click="openAddNumberDialog">New Entity</v-btn>
+    </v-toolbar>
     <v-data-table-server :items="entities" @update:options="requestServerItems" :items-per-page-options="[15, 20, 25]"
         :headers="headers" v-model:items-per-page="itemsPerPage" :items-length="totalItems" :search="search">
-        <template v-slot:top>
-            <v-toolbar flat>
-                <v-text-field class="ml-1" v-model="search" append-inner-icon="mdi-magnify" label="Search" single-line
-                    hide-details></v-text-field>
-                <v-divider class="mx-4" inset vertical></v-divider>
-                <v-btn color="green" prepend-icon="mdi-sim" @click="openAddNumberDialog">New Entity</v-btn>
-            </v-toolbar>
-        </template>
         <template v-slot:item.networkIdentifier="{ item }">
             <v-icon v-if="item.deviceType == 'MOBILE'" color="green">mdi-phone</v-icon>
             <v-icon v-else color="blue">mdi-router-wireless</v-icon>
@@ -19,7 +17,9 @@
             <v-icon v-else color="red">mdi-close</v-icon>
         </template>
         <template v-slot:item.owner="{ item }">
-            <v-chip v-if="item.owner" color="primary" @click="router.push({ name: 'Customer', params: { id: item.owner.id } });">{{ item.owner.firstName + ' ' + item.owner.lastName }}</v-chip>
+            <v-chip v-if="item.owner" color="primary"
+                @click="router.push({ name: 'Customer', params: { id: item.owner.id } });">{{ item.owner.firstName + ' '
+            + item.owner.lastName }}</v-chip>
             <v-chip v-else color="grey" text>Unassigned</v-chip>
         </template>
     </v-data-table-server>
@@ -36,7 +36,7 @@
                         <div v-if="!responseObj.loading" class="pa-4 text-center">
                             <v-icon size="80" color="green">mdi-check</v-icon>
                             <h3 class="text-h6 font-weight-light mb-2">{{ responseObj.success ? 'Success' :
-                                'Something went wrong' }}!</h3>
+            'Something went wrong' }}!</h3>
                             <span class="text-caption text-grey">{{ responseObj.message }}</span>
                         </div>
                         <v-progress-linear v-else indeterminate color="primary"></v-progress-linear>
@@ -120,6 +120,6 @@ const assignEntity = async (entityId, entity) => {
         responseObj.value.message = 'Something went wrong'
         console.log(error)
     })
-    
+
 }
 </script>
