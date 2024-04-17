d9ad484f02e092a1649d1c877763d73799052069
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-04-18 00:32:01 +0200

    remove error logs

--- src/services/rest/auth-api.js
+++ src/services/rest/auth-api.js
@@ -15,13 +15,7 @@ export const authorizeUser = async (username, password) => {
         body: new URLSearchParams(formData)
     })
         .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
             return response.json();
-        })
-        .catch(error => {
-            console.error('There was a problem with your fetch operation:', error);
         });
 }
 
