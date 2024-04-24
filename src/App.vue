5967cfd6341878047411eb7bb999596b6a98f2cd
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-04-24 22:57:47 +0200

    add snackbar element to the root element

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,20 @@
   <v-app>
     <v-main>
       <router-view/>
+      <v-snackbar 
+      v-model="snackbarStore.show" varian="tonal" 
+      :timeout="snackbarStore.timeout"
+      :color="snackbarStore.color"
+      >
+      <v-icon>{{ snackbarStore.icon }}</v-icon>
+        {{ snackbarStore.text }}
+      </v-snackbar>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
+import { useSnackbarStore } from "@/stores/SnackBarStore"
 
+const snackbarStore = useSnackbarStore()
 </script>
