d4002329aea4e7e1a8547e3da1cf1e2b17a66714
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-04-27 16:01:56 +0200

    add close button to the snackbar element

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      <router-view/>
-      <v-snackbar 
-      v-model="snackbarStore.show" varian="tonal" 
-      :timeout="snackbarStore.timeout"
-      :color="snackbarStore.color"
-      >
-      <v-icon>{{ snackbarStore.icon }}</v-icon>
+      <router-view />
+      <v-snackbar v-model="snackbarStore.show" varian="tonal" :timeout="snackbarStore.timeout"
+        :color="snackbarStore.color">
+        <v-icon>{{ snackbarStore.icon }}</v-icon>
         {{ snackbarStore.text }}
+        <template v-slot:actions>
+          <v-btn color="blue" variant="icon" icon="mdi-close" @click="snackbarStore.show = false">
+          </v-btn>
+        </template>
       </v-snackbar>
     </v-main>
   </v-app>
