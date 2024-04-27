fcb53ae7373db8f7b83ec849d03240ef4727ae4b
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-04-27 23:19:34 +0200

    add hardcoded data in dashboard

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,78 +1,100 @@
 <template>
   <v-container class="pb-0">
-        <div class="text-h5">Dashboard</div>
-    </v-container>
-  <v-container>
-    
+    <div class="text-h5">Dashboard</div>
   </v-container>
+  <v-container>
+    <v-card>
+      <v-card-title class="text-overline">
+        Active users
+
+        <div class="text-primary text-h3 font-weight-bold">90%</div>
+
+        <div class="text-h6 text-medium-emphasis font-weight-regular">
+          2,938 remaining
+        </div>
+      </v-card-title>
+      <br>
+      <v-card-text>
+        <div :style="`right: calc(${review} - 32px)`" class="position-absolute mt-n8 text-caption text-primary">
+          Activity review
+        </div>
+        <v-progress-linear color="primary" height="22" model-value="90" rounded="lg">
+        </v-progress-linear>
+
+        <div class="d-flex justify-space-between py-3">
+          <span class="text-primary font-weight-medium">
+            26,442 active
+          </span>
+
+          <span class="text-medium-emphasis" text-color="primary"> 29,380 total </span>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-container>
+  <v-container>
+    <v-card>
+      <v-card-title class="text-overline">
+        Revenue
+
+        <div class="text-primary text-h3 font-weight-bold">+$250,000</div>
+
+        <div class="text-h6 text-medium-emphasis font-weight-regular">
+          $50,000 remaining
+        </div>
+      </v-card-title>
+      <br>
+      <v-card-text>
+        <div :style="`right: calc(${review} - 32px)`" class="position-absolute mt-n8 text-caption text-primary">
+          Revenue review
+        </div>
+        <v-progress-linear color="primary" height="22" model-value="80" rounded="lg">
+        </v-progress-linear>
+
+        <div class="d-flex justify-space-between py-3">
+          <span class="text-primary font-weight-medium">
+            $200,000 earned
+          </span>
+
+          <span class="text-medium-emphasis" text-color="primary">$250,000 target</span>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-container>
+
+  <v-container>
+    <v-card>
+      <v-card-title class="text-overline">
+        Employees
+
+        <div class="text-primary text-h3 font-weight-bold">+3</div>
+
+        <div class="text-h6 text-medium-emphasis font-weight-regular">
+          2 positions open
+        </div>
+      </v-card-title>
+      <br>
+      <v-card-text>
+        <div :style="`right: calc(${review} - 32px)`" class="position-absolute mt-n8 text-caption text-primary">
+          Employee review
+        </div>
+        <v-progress-linear color="primary" height="22" model-value="70" rounded="lg">
+        </v-progress-linear>
+
+        <div class="d-flex justify-space-between py-3">
+          <span class="text-primary font-weight-medium">
+            37 employees
+          </span>
+
+          <span class="text-medium-emphasis" text-color="primary">40 positions</span>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-container>
+
 </template>
 
 <script setup>
-import { ref } from 'vue';
-const actions = ref([
-  {
-    "action": "User Management",
-    "features": [
-      "Add, modify, or remove user accounts",
-      "Modify access control"
-    ]
-  },
-  {
-    "action": "Plan Management",
-    "features": [
-      "Create, update, and delete subscription plans",
-      "View and analyze plan performance"
-    ]
-  },
-  {
-    "action": "Billing Information",
-    "features": [
-      "Display billing details, payment history, and invoices",
-      "Process payments and view pending payments"
-    ]
-  },
-  {
-    "action": "Network Analytics",
-    "features": [
-      "Show real-time analytics and performance metrics",
-      "Graphs and charts for data usage, network speed, and reliability"
-    ]
-  },
-  {
-    "action": "Customer Support",
-    "features": [
-      "Customer support",
-      "Ticketing system for tracking and resolving concerns"
-    ]
-  },
-  {
-    "action": "Inventory Management",
-    "features": [
-      "Maintain inventory of telecom equipment and devices",
-      "Track stock levels and order new inventory"
-    ]
-  },
-  {
-    "action": "Notifications and Alerts",
-    "features": [
-      "Notifications"    
-    ]
-  },
-  {
-    "action": "Reports and Analytics",
-    "features": [
-      "Provide detailed reports on user activities, network performance, and finances",
-      "Filters and customization options for specific reports"
-    ]
-  },
-  {
-    "action": "System Settings",
-    "features": [
-      "Configure system settings",
-      "Customization options like themes or language preferences"
-    ]
-  }
-]);
+
 </script>
 
 <style></style>
