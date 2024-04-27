eb1c74f5713670d679ddc8b17b0792ec5cf69f74
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-04-27 16:09:35 +0200

    add list of allowed roles for authenticated pages

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,82 +1,119 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useSnackbarStore } from '@/stores/SnackBarStore'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/HomeView.vue')
+    component: () => import('../views/HomeView.vue'),
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/LoginView.vue')
+    component: () => import('../views/LoginView.vue'),
   },
   {
     path: '/subscriptions',
     name: 'Subscriptions',
-    component: () => import('../views/SubscriptionsView.vue')
+    component: () => import('../views/SubscriptionsView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales', 'agent']
+    }
 
   },
   {
     path: '/sales',
     name: 'Sales',
-    component: () => import('../views/SalesView.vue')
+    component: () => import('../views/SalesView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales']
+    }
   },
   {
     path: '/customers',
     name: 'Customers',
-    component: () => import('../views/CustomersView.vue')
+    component: () => import('../views/CustomersView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'agent']
+    }
   },
   {
     path: '/customers/:id',
     name: 'Customer',
-    component: () => import('../views/CustomerView.vue')
+    component: () => import('../views/CustomerView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales', 'agent']
+    }
   },
   {
     path: '/plans',
     name: 'Plans',
-    component: () => import('../views/PlansView.vue')
+    component: () => import('../views/PlansView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales', 'agent']
+    }
   },
   {
     path: '/devices',
     name: 'Devices',
-    component: () => import('../views/DevicesView.vue')
+    component: () => import('../views/DevicesView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales', 'agent']
+    }
   },
   {
     path: '/settings',
     name: 'Settings',
-    component: () => import('../views/SettingsView.vue')
+    component: () => import('../views/SettingsView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales', 'agent']
+    }
   },
   {
     path: '/my-profile',
     name: 'MyProfile',
-    component: () => import('../views/MyProfileView.vue')
+    component: () => import('../views/MyProfileView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales', 'agent']
+    }
   },
   {
     path: '/services',
     name: 'Services',
-    component: () => import('../views/ServicesView.vue')
+    component: () => import('../views/ServicesView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales', 'agent']
+    }
   },
   {
     path: '/promotions',
     name: 'Promotions',
-    component: () => import('../views/PromotionsView.vue')
+    component: () => import('../views/PromotionsView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales']
+    }
   },
   {
     path: '/offered-promotions',
     name: 'OfferedPromotions',
-    component: () => import('../views/OfferedPromotionsView.vue')
-  },{
+    component: () => import('../views/OfferedPromotionsView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'sales']
+    }
+  }, {
     path: '/network-entities',
     name: 'NetworkEntities',
-    component: () => import('../views/NetworkEntitiesView.vue')
+    component: () => import('../views/NetworkEntitiesView.vue'),
+    meta: {
+      roleAllowed: ['admin', 'agent']
+    }
   },
   {
     path: '/error',
     name: 'Forbidden',
     props: {
-      errorMessage: "Access Denied", 
-      errorStatus: "403", 
+      errorMessage: "Access Denied",
+      errorStatus: "403",
       errorText: "SORRY, YOU DON'T HAVE THE PERMISSION TO ACCESS THIS PAGE"
     },
     component: () => import('../views/ErrorTemplate.vue')
@@ -85,8 +122,8 @@ const routes = [
     path: '/error',
     name: 'ServiceUnavailable',
     props: {
-      errorMessage: "Service Unavailable", 
-      errorStatus: "503", 
+      errorMessage: "Service Unavailable",
+      errorStatus: "503",
       errorText: "SORRY, THE SERVICE YOU ARE TRYING TO ACCESS IS CURRENTLY UNAVAILABLE"
     },
     component: () => import('../views/ErrorTemplate.vue')
@@ -95,8 +132,8 @@ const routes = [
     path: '/error',
     name: 'Unhandled',
     props: {
-      errorMessage: "An error occurred", 
-      errorStatus: "???", 
+      errorMessage: "An error occurred",
+      errorStatus: "???",
       errorText: "SORRY, AN ERROR OCCURRED WE HAVEN'T HANDLED YET"
     },
     component: () => import('../views/ErrorTemplate.vue')
@@ -105,8 +142,8 @@ const routes = [
     path: '/:catchAll(.*)',
     name: 'NotFound',
     props: {
-      errorMessage: "This page could not be found", 
-      errorStatus: "404", 
+      errorMessage: "This page could not be found",
+      errorStatus: "404",
       errorText: "SORRY, BUT THE PAGE YOU ARE LOOKING FOR DOES NOT EXIST, HAVE BEEN REMOVED, NAME CHANGED OR IS TEMPORARILY UNAVAILABLE"
     },
     component: () => import('../views/ErrorTemplate.vue')
@@ -118,8 +155,20 @@ const router = createRouter({
   routes
 })
 
-router.afterEach((to, from) => {
-  document.title = to.name
+router.beforeEach((to, from) => {
+  if (!to.meta.roleAllowed || (to.meta.roleAllowed && isAllowed(to.meta.roleAllowed))) {
+    document.title = to.name
+  }else{
+    return { name: 'Forbidden'};
+  }
 })
+
+function isAllowed(rolesAlowed) {
+  var user = JSON.parse(localStorage.getItem("user"));
+  var setA = new Set(user.roles);
+  var setB = new Set(rolesAlowed);
+  var intersection = new Set([...setA].filter(x => setB.has(x)));
+  return Array.from(intersection).length > 0;
+}
 
 export default router
