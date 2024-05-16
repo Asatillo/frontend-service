2875db5d30c8b299f2a14046d8ca6285fec8253c
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-05-16 14:04:30 +0200

    roll back

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <Navbar />
+    <!-- Navbar -->
     <v-main>
       <router-view />
       <v-snackbar v-model="snackbarStore.show" varian="tonal" :timeout="snackbarStore.timeout"
@@ -8,7 +8,7 @@
         <v-icon>{{ snackbarStore.icon }}</v-icon>
         {{ snackbarStore.text }}
         <template v-slot:actions>
-          <v-btn color="blue" variant="icon" icon="mdi-close" @click="snackbarStore.show = false">
+          <v-btn color="blue" prepend-icon="mdi-close" @click="snackbarStore.show = false">
           </v-btn>
         </template>
       </v-snackbar>
@@ -18,15 +18,6 @@
 
 <script setup>
 import { useSnackbarStore } from "@/stores/SnackBarStore"
-import Navbar from "@/components/Navbar.vue"
 
 const snackbarStore = useSnackbarStore()
 </script>
-
-<style>
-/* TODO: place it to the global css file */
-.v-table {
-    border-radius: 13px;
-    padding: 10px;
-}
-</style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,12 @@
+<!-- Layout.vue -->
 <template>
+  <Navbar />
   <v-container style="max-width: 100%;">
     <slot></slot>
   </v-container>
 </template>
+  
+<script setup>
+import Navbar from "@/components/Navbar.vue"
+</script>
   
